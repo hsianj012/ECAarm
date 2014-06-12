2013081678af2ec2463b16fb35d5f32e583d5e87
Judy Hsiang <jhsiang@mit.edu>
2014-06-12 10:13:26 -0400

    updated to use actual geometry of arm. still needs some fixing

--- pointToAngle.m
+++ pointToAngle.m
@@ -1,23 +1,45 @@
 function [slew, shoulder, elbow] = pointToAngle(x,y,z)
 
-    % fixed distances <-- needs to be measured
-    forarm = 8; % jaw to elbow
-    midarm = 9; % elbow to shoulder
-%     aftarm = 0; % shoulder to slew
+    %% Points of Interest
+    % O = origin or slew pivot pt
+    % A = shoulder pivot pt
+    % B = end of "bicep"
+    % C = elbow pivot pt
+    % D = start of forearm
+    % E = end of forarm/middle of jaw
     
-    % physical limits of arm
+    
+    % Distances between POI
+    OA = 3.5;
+    bicep = 9; % distace AB
+    BC = 2.25;
+    CD = 3;
+    forearm = 10.5; % distance DE
+
+    CE = sqrt(forearm^2 + CD^2);
+    AC = 9+(5/32);
+    
+    
+    %% physical limits of arm
 %     MINshould = 0;
 %     MAXshould = 90;
 %     MINelbow = 0;
 %     MAXelbow = 130;
 
+    %% angles
     slew = atand(x/y);
     
-    r = sqrt(x^2 + y^2);
+    r = sqrt(x^2 + y^2)-3;
     l = sqrt(r^2 + z^2);
-    % shoulder 
-%     minshoulder = atand(z,r);
     
-    shoulder = atand(z/r) + acosd((forarm^2 - midarm^2 - l^2)/(-2*midarm*l));
-    elbow = acosd((l^2 - midarm^2 - forarm^2)/(-2*midarm*forarm));
+    CAE = acosd((CE^2 - AC^2 - l^2)/(-2*AC*l));
+    BAC = acosd((BC^2 - bicep^2 - AC^2)/(-2*bicep*AC));
+    shoulder = CAE + BAC + atand(z/r);
+    
+    CEA = acosd((AC^2 - l^2 - CE^2)/(-2*l*CE));
+    DEC = atand(CD/forearm);
+    elbow = 180 - (CEA + DEC + CAE + BAC);
 end
+
+
+
