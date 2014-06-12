284b796396216e24207b9b558a3b1dde62218ec3
Judy Hsiang <jhsiang@mit.edu>
2014-06-12 10:06:26 -0400

    updated this to reflect actual geometry of the arm. Has variable out of x,y,z position of jaw and optionally the points of interest (r,z) of each arm segment. POI is used for illustrating position of arm.

--- angleToPoint.m
+++ angleToPoint.m
@@ -1,29 +1,57 @@
-function [x,y,z] = angleToPoint(slew,shoulder,elbow)
     % This functions takes angles (in degrees) and maps it to
     % a point in cartesian coordinates
 
-    % fixed distances <-- needs to be measured
-    forarm = 8; % jaw to elbow
-    midarm = 9; % elbow to shoulder
-%     aftarm = 0; % shoulder to slew
-    
-   
-    shoulderPos.r = midarm*cosd(shoulder);
-    shoulderPos.z = midarm*sind(shoulder);
-    
-    angle = 180-elbow-shoulder;
-    elbowPos.r = forarm*cosd(angle);
-    elbowPos.z = -forarm*sind(angle);
-    
-    r.arm = shoulderPos.r + elbowPos.r;
-    
-    r.x = -r.arm*sind(slew);
-    r.y = -r.arm*cosd(slew);
+    %% Points of Interest
+    % O = origin or slew pivot pt
+    % A = shoulder pivot pt
+    % B = end of "bicep"
+    % C = elbow pivot pt
+    % D = start of forearm
+    % E = end of forarm/middle of jaw
     
     
-    x = r.x;
-    y = r.y;
-    z = shoulderPos.z + elbowPos.z;
+    % Distances between POI
+    OA = 3.5;
+    bicep = 9; % distace AB
+    BC = 2.25;
+    CD = 3;
+    forearm = 10.5; % distance DE
+    
+    %% Determine (r,z) for each POI
+    O.r = 0; O.z = 0;
+    
+    A.r = OA; A.z = 0;
+    
+    B.r = bicep*cosd(shoulder);
+    B.z = bicep*sind(shoulder);
+    
+    % angle ABC - law of cosines
+    AC = 9+(5/32);
+    phi = acosd((AC^2 - bicep^2 - BC^2)/(-2*bicep*BC));
+    
+    thetaC = 180-shoulder-phi;
+    C.r = BC*cosd(thetaC);
+    C.z = -BC*sind(thetaC);
+    
+    thetaD = 90-shoulder-elbow;
+    D.r = CD*cosd(thetaD);
+    D.z = -CD*sind(thetaD);
+    
+    thetaElbow = 180-elbow-shoulder;
+    E.r = forearm*cosd(thetaElbow);
+    E.z = -forearm*sind(thetaElbow);
+    
+    r.arm = O.r + A.r + B.r + C.r + D.r +E.r;
+    
+    r.x = r.arm*sind(slew);
+    r.y = r.arm*cosd(slew);
+    
+    nOutputs = nargout;
+    varargout = cell(1,nOutputs);
+    varargout{1} = r.x; %x
+    varargout{2} = r.y; %y
+    varargout{3} = O.z + A.z + B.z + C.z + D.z +E.z;%z
+    varargout{4} = [O,A,B,C,D,E]; %POI
 end
     
     
