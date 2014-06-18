81d8ece88783f9a4f92fd227948a2d988c63d81b
Judy Hsiang <jhsiang@mit.edu>
2014-06-18 11:01:23 -0400

    tests angleToPoint.m and pointToAngle.m and plots the results.

--- testPtConv.m
+++ testPtConv.m
@@ -2,15 +2,16 @@ clear all
 % make each motor sweep from 0 - 90 degrees, one at a time.
 emptySet = zeros(1,10);
 zero2nine = linspace(0,90,10);
-alpha = [emptySet,emptySet,zero2nine]; % slew 
-gamma = [emptySet,zero2nine,90*ones(1,10)]; % shoulder
-beta = [zero2nine,90*ones(1,20)]; % elbow
+alpha = [emptySet,zero2nine]; % slew 
+gamma = [zero2nine,90*ones(1,10)]; % shoulder
+beta = [emptySet,emptySet]; % elbow
 
 l = length(alpha);
 
 x = zeros(1,l);
 y = zeros(1,l);
 z = zeros(1,l);
+r = zeros(1,l);
 slew = zeros(1,l);
 shoulder =zeros(1,l);
 elbow = zeros(1,l);
@@ -18,24 +19,28 @@ elbow = zeros(1,l);
 % (x,y,z) for each set of angles
 for i=1:l
     [x(i),y(i),z(i)] = angleToPoint(alpha(i),gamma(i),beta(i));
+    r(i) = sqrt(x(i)^2+y(i)^2+z(i)^2);
 end
 
 % 3D plot of where the jaw has moved.
-subplot(1,2,1)
+figure(1)
 plot3(x,y,z)
 axis equal
 grid on
 title('jaw movement')
-
+xlabel('x location')
+ylabel('y location')
+zlabel('z location')
 
 
 for j=1:l
     [slew(j),shoulder(j),elbow(j)]=pointToAngle(x(j),y(j),z(j));
 end
-subplot(1,2,2)
+
+figure(2)
 index = 1:l;
-plot(index,slew,'b',index,shoulder,'r',index,elbow,'g', [0 30],[90 90],'m')
+target = 90*ones(1,l);
+plot(index,slew,'b',index,shoulder,'r',index,elbow,'g', index,target,'m:')
 title('Joint Angles')
+ylabel('Angle (degrees)')
 legend('slew','shoulder','elbow')
-
-
