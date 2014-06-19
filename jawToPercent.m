c44af9ec94023172ba61e4c775944d0a01ac9ca4
Judy Hsiang <jhsiang@mit.edu>
2014-06-19 16:58:33 -0400

    converts jaw position from packet to percentage open

---
+++ jawToPercent.m
@@ -0,0 +1,8 @@
+function [ percent_open ] = jawToPercent(jaw_pos)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+    k = 66;
+    percent_open = jaw_pos/k;
+end
+
