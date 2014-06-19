a4051317db484f3b30aae7986a01e04a3174e7a0
Judy Hsiang <jhsiang@mit.edu>
2014-06-19 16:57:31 -0400

    added comments and placeholders for more functionality in the future

--- ArmControlGUI.m
+++ ArmControlGUI.m
@@ -22,7 +22,7 @@ function varargout = ArmControlGUI(varargin)
 
 % Edit the above text to modify the response to help ArmControlGUI
 
-% Last Modified by GUIDE v2.5 18-Jun-2014 17:29:51
+% Last Modified by GUIDE v2.5 19-Jun-2014 14:51:58
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -54,6 +54,7 @@ function ArmControlGUI_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for ArmControlGUI
 handles.output = hObject;
+handles.input = zeros(1,5);
 
 % Update handles structure
 guidata(hObject, handles);
@@ -80,15 +81,19 @@ function inputTypePanel_SelectionChangeFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+% update text on GUI based on type of input user wants to use
 if (hObject == handles.anglesBtn)
     set(handles.text1, 'String', 'Shoulder:');
     set(handles.text2, 'String', 'Slew:');
     set(handles.text3, 'String', 'Elbow:');
+    handles.inputType = 'angles';
 else
     set(handles.text1, 'String', 'X:');
     set(handles.text2, 'String', 'Y:');
     set(handles.text3, 'String', 'Z:');
+    handles.inputType = 'point';
 end
+guidata(hObject, handles);
 
 function textInput1_Callback(hObject, eventdata, handles)
 % hObject    handle to textInput1 (see GCBO)
@@ -97,6 +102,8 @@ function textInput1_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of textInput1 as text
 %        str2double(get(hObject,'String')) returns contents of textInput1 as a double
+
+% store value of x or shoulder angle as input(1)
 input1 = str2double(get(hObject, 'String'));
 if isnan(input1)
     set(hObject, 'String', 0);
@@ -127,6 +134,8 @@ function textInput2_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of textInput2 as text
 %        str2double(get(hObject,'String')) returns contents of textInput2 as a double
+
+% store value of y or slew angle as input(2)
 input2 = str2double(get(hObject, 'String'));
 if isnan(input2)
     set(hObject, 'String', 0);
@@ -148,8 +157,6 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
-
-
 function textInput3_Callback(hObject, eventdata, handles)
 % hObject    handle to textInput3 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -157,6 +164,8 @@ function textInput3_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of textInput3 as text
 %        str2double(get(hObject,'String')) returns contents of textInput3 as a double
+
+% store value of z or elbow angle as input(3)
 input3 = str2double(get(hObject, 'String'));
 if isnan(input3)
     set(hObject, 'String', 0);
@@ -178,31 +187,68 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+
+% --- Executes on slider movement.
+function jawSlider_Callback(hObject, eventdata, handles)
+% hObject    handle to jawSlider (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+% Store jaw open percentage
+jawValue = get(hObject,'Value');
+handles.input(5) = jawValue;
+
+% display current position of slider on GUI
+set(handles.jawVal,'String',num2str(jawValue));
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function jawSlider_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to jawSlider (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
 % --- Executes on button press in runBtn.
 function runBtn_Callback(hObject, eventdata, handles)
 % hObject    handle to runBtn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% set demand type to position for shoulder, slew, and elbow
 handles.demandType = [5 5 5 0 0];
-handles.input(4:5) = [5 6];
-axes(handles.axes2);
-cla
-set(handles.status, 'String', 'RUNNING...');
-set(handles.statusBG, 'BackgroundColor',[1 1 0]);
-set(handles.status,'BackgroundColor',get(handles.statusBG,'BackgroundColor'));
-pause(5);
-plot(handles.input,handles.demandType)
+
+% check for jaw open/close command and set demand type to position if
+% present
+if handles.input(5)
+    handles.demandType(5) = 5;
+end
+
+% update status to 'running' on GUI
+if get(handles.runBtn,'Value')
+    set(handles.status, 'String', 'RUNNING...');
+    set(handles.statusBG, 'BackgroundColor',[1 1 0]);
+    set(handles.status,'BackgroundColor',get(handles.statusBG,'BackgroundColor'));
+end
+
+% run function
+inputType = handles.inputType;
+input = handles.input;
+demand_type = handles.demandType;
+[handles.position,handles.current] = runArm(inputType, input, demand_type);
+
+% update status to 'complete' on GUI
 set(handles.status, 'String', 'COMPLETE');
 set(handles.statusBG, 'BackgroundColor',[0 1 0]);
 set(handles.status,'BackgroundColor',get(handles.statusBG,'BackgroundColor'));
 
-
-% --- Executes on button press in stopBtn.
-function stopBtn_Callback(hObject, eventdata, handles)
-% hObject    handle to stopBtn (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-handles.demandType = [0 0 0 0 0];
-set(handles.status, 'String', 'STOPPED!')
-set(handles.statusBG, 'BackgroundColor',[1 0 0]);
-set(handles.status,'BackgroundColor',get(handles.statusBG,'BackgroundColor'));
+guidata(hObject, handles);
+% end
