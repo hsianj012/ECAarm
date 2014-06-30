fa7da9f2b29468aa2e52a719afb338d90eb4183f
Judy Hsiang <jhsiang@mit.edu>
2014-06-30 16:50:01 -0400

    Function that reads lvm file from labview and returns the force and torque values.

---
+++ lvmToFT.m
@@ -0,0 +1,71 @@
+function [Fx, Fy, Fz, Tx, Ty, Tz] = lvmToFT(filename)
+%lvmToFT this function takes in labview lvm file formatted as a (two-column csv)
+% and returns the force and torque data
+
+
+%% Initialize variables.
+delimiter = ',';
+% % If file has headers
+startRow = 23;
+% % if no headers
+% startRow = 1; 
+endRow = inf;
+
+%% Format string for each line of text:
+%   column2: double (%f)
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%*s%f%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(filename,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines', startRow(1)-1, 'ReturnOnError', false);
+for block=2:length(startRow)
+    frewind(fileID);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines', startRow(block)-1, 'ReturnOnError', false);
+    dataArray{1} = [dataArray{1};dataArrayBlock{1}];
+end
+
+%% Close the text file.
+fclose(fileID);
+
+%% Post processing for unimportable data.
+% No unimportable data rules were applied during the import, so no post
+% processing code is included. To generate code which works for
+% unimportable data, select unimportable cells in a file and regenerate the
+% script.
+
+%% Allocate imported array to column variable names
+FTData = dataArray{:, 1};
+dataLength = size(FTData,1)/6;
+
+% initiate vectors
+Fx = zeros(1,dataLength);
+Fy = zeros(1,dataLength);
+Fz = zeros(1,dataLength);
+Tx = zeros(1,dataLength);
+Ty = zeros(1,dataLength);
+Tz = zeros(1,dataLength);
+
+% fill with data from file
+for i = 1:(dataLength-1)
+    Fx(i)=FTData(6*(i-1)+1);
+    Fy(i)=FTData(6*(i-1)+2);
+    Fz(i)=FTData(6*(i-1)+3);
+    Tx(i)=FTData(6*(i-1)+4);
+    Ty(i)=FTData(6*(i-1)+5);
+    Tz(i)=FTData(6*(i-1)+6);
+end
+
+%% Comment this section out if you want each pt instead of average
+% Return average force/torques
+Fx = mean(Fx);
+Fy = mean(Fy);
+Fz = mean(Fz);
+Tx = mean(Tx);
+Ty = mean(Ty);
+Tz = mean(Tz);
