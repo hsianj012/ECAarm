3a93cd9fdd08105e73177fbc1a68869e364b2cad
Judy Hsiang <jhsiang@mit.edu>
2014-07-16 16:18:17 -0400

    forgot why k = 66, so i added a comment

--- jawToPercent.m
+++ jawToPercent.m
@@ -2,7 +2,7 @@ function [ percent_open ] = jawToPercent(jaw_pos)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-    k = 66;
+    k = 66; % range is smaller for jaw
     percent_open = jaw_pos/k;
 end
 
--- percentToJaw.m
+++ percentToJaw.m
@@ -1,8 +1,9 @@
+
 function [ jaw_pos ] = percentToJaw(percent_open)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-    k = 66;
+    k = 66; % range is smaller for jaw
     jaw_pos = k*percent_open;
 
 end
