4f468334ede44c8a8ec14dbc6ee4190407f68dcc
Judy Hsiang <jhsiang@mit.edu>
2014-07-22 16:30:23 -0400

    changed data structure for position from struct to matrix. Fixed byte order of data packet read

--- dataForPlot.m
+++ dataForPlot.m
@@ -6,6 +6,8 @@ if (~isempty(data()))
      position = zeros(5,M);
      current = zeros(5,M);
      speed = zeros(5,M);
+     posAng = zeros(4,M);
+     posPt = zeros(3,M);
      
      offset_position = 6;
      offset_speed = 8;
@@ -15,23 +17,23 @@ if (~isempty(data()))
      for i=1:5
        for j =1:M
          % i 
-         position(i,j) = 256*data(offset_position + (i-1)*width,j) + ...
-              data(offset_position + (i-1)*width + 1, j);
-         current(i,j) = 256*data(offset_current + (i-1)*width,j) + ...
-              data(offset_current + (i-1)*width + 1, j);
-         speed(i,j) = 256*data(offset_speed + (i-1)*width,j) + ...
-              data(offset_speed + (i-1)*width + 1, j);
+         position(i,j) = data(offset_position + (i-1)*width,j) + ...
+              256*data(offset_position + (i-1)*width + 1, j);
+         current(i,j) = data(offset_current + (i-1)*width,j) + ...
+              256*data(offset_current + (i-1)*width + 1, j);
+         speed(i,j) = data(offset_speed + (i-1)*width,j) + ...
+              256*data(offset_speed + (i-1)*width + 1, j);
        end
      end
 
-     posAng.shoulder = shoulderToAngle(position(1,:));
-     posAng.slew = slewToAngle(position(2,:));
-     posAng.elbow = elbowToAngle(position(3,:));
+     posAng(1,:) = shoulderToAngle(position(1,:));
+     posAng(2,:) = slewToAngle(position(2,:));
+     posAng(3,:) = elbowToAngle(position(3,:));
 %      posAng.wrist = 
-     posAng.jaw = jawToPercent(position(5,:));
+     posAng(4,:) = jawToPercent(position(5,:));
      
      for k = 1:M
-         [posPt.x(k),posPt.y(k),posPt.z(k)]=angleToPoint(posAng.shoulder(k), posAng.slew(k), posAng.elbow(k));
+         [posPt(1,k),posPt(2,k),posPt(3,k)]=angleToPoint(posAng(1,k), posAng(2,k), posAng(3,k));
      end
      
 %      hold on
